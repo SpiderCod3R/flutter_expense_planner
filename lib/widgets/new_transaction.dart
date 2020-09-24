48bdb781e8e1ce807e79602580a5d6ea019f9669
Thunder Master <thndrmaster@gmail.com>
2020-09-24 16:14:53 -0300

    Transformando NewTransaction de Stateless para StateFul e Fechando o Bottom Sheet com ( Navigator )

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  final titleController = TextEditingController();
-  final ammountController = TextEditingController();
+class NewTransaction extends StatefulWidget {
   final Function addNewTransaction;
 
   NewTransaction(this.addNewTransaction);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final ammountController = TextEditingController();
 
   void submitTransaction() {
     final titleInput = titleController.text;
@@ -15,10 +22,12 @@ class NewTransaction extends StatelessWidget {
       return;
     }
 
-    addNewTransaction(
+    widget.addNewTransaction(
       titleInput,
       ammountInput,
     );
+
+    Navigator.of(context).pop();
   }
 
   @override
