3a4e0a1b587ff2d7f7394a7c771bc0800d307f56
Thunder Master <thndrmaster@gmail.com>
2020-09-28 11:28:05 -0300

    Criando e Adicionando Datepicker em new transactions

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addNewTransaction;
@@ -10,13 +11,13 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _ammountController = TextEditingController();
+  DateTime _selectedDate;
 
-  final ammountController = TextEditingController();
-
-  void submitTransaction() {
-    final titleInput = titleController.text;
-    final ammountInput = double.parse(ammountController.text);
+  void _submitTransaction() {
+    final titleInput = _titleController.text;
+    final ammountInput = double.parse(_ammountController.text);
 
     if (titleInput.isEmpty || ammountInput <= 0) {
       return;
@@ -30,6 +31,29 @@ class _NewTransactionState extends State<NewTransaction> {
     Navigator.of(context).pop();
   }
 
+  void _presetDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime.now(),
+      builder: (context, child) {
+        return Theme(
+          data: ThemeData.dark(),
+          child: child,
+        );
+      },
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+    print("...");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -40,24 +64,44 @@ class _NewTransactionState extends State<NewTransaction> {
           crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
             TextField(
-                decoration: InputDecoration(labelText: 'Title'),
-                controller: titleController,
-                onSubmitted: (_) => submitTransaction()
-                // onChanged: (val) {
-                //   titleInput = val;
-                // },
-                ),
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: _titleController,
+              onSubmitted: (_) => _submitTransaction(),
+            ),
             TextField(
               decoration: InputDecoration(labelText: 'Ammout'),
-              controller: ammountController,
+              controller: _ammountController,
               keyboardType: TextInputType.number,
-              onSubmitted: (_) => submitTransaction(),
+              onSubmitted: (_) => _submitTransaction(),
               // onChanged: (val) => ammoutInput = val,
             ),
-            FlatButton(
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? 'Sem data Selecionada'
+                          : 'Data Selecionada - ${DateFormat.yMd('pt').format(_selectedDate)}',
+                    ),
+                  ),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: _presetDatePicker,
+                  )
+                ],
+              ),
+            ),
+            RaisedButton(
                 child: Text("Add Transaction"),
-                textColor: Colors.purple,
-                onPressed: submitTransaction),
+                color: Theme.of(context).primaryColor,
+                textColor: Theme.of(context).textTheme.button.color,
+                onPressed: _submitTransaction),
           ],
         ),
       ),
